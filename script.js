ca6a3205c81100cc7120910f1183e0ef37820d63
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-01-19 19:40:03 -0500

    added password options modal, implemented dictionary to keep track of selected password character types

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 // Assignment Code
+var passwordParameters= {"lowercase": false, "uppercase": false, "number": false, "special": false}
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function toggleLowercase() {
+  var checkBox= document.getElementById("lowercaseCheck")
+  if (checkBox.checked == true){
+    passwordParameters["lowercase"] = true
+  } else if (checkBox.checked == false){
+    passwordParameters["lowercase"] = false
+  }
+}
+
+function toggleUppercase() {
+  var checkBox= document.getElementById("uppercaseCheck")
+  if (checkBox.checked == true){
+    passwordParameters["uppercase"] = true
+  } else if (checkBox.checked == false){
+    passwordParameters["uppercase"] = false
+  }
+}
+
+function toggleNumber() {
+  var checkBox= document.getElementById("numberCheck")
+  if (checkBox.checked == true){
+    passwordParameters["number"] = true
+  } else if (checkBox.checked == false){
+    passwordParameters["number"] = false
+  }
+}
+
+function toggleSpecial() {
+  var checkBox= document.getElementById("specialCheck")
+  if (checkBox.checked == true){
+    passwordParameters["special"] = true
+  } else if (checkBox.checked == false){
+    passwordParameters["special"] = false
+  }
+}
+
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
